f4639920b879e0948f87b0819ce162d438719534
Cleyton <professor.cleyton.mach1@gmail.com>
2023-02-27 20:46:09 -0300

    Aula ao vivo 27-02

--- script.js
+++ script.js
@@ -6,11 +6,16 @@ let listaDeConvidados = ['Cleyton', 'Dimas']
 
 /* Identificando IDS que serão manipulados na tela */
 let tabelaListaConvidados = document.getElementById('lista-convidados')
+let inputNomeConvidado = document.querySelector('#nome-convidado')
+/* Dessa forma retorna um HTML collection */
+// let botaoIncluirConvidado = document.getElementsByClassName('btn-primary')
+let botaoIncluirConvidado = document.getElementById('botao-incluir')
+let divMensagemUsuario = document.getElementById('mensagem-usuario')
 
 /* Função que insere registro a registro de usuário na lista (dentro da tabela do HTML) */
 function insereConvidadoNoHTML (convidado) {
   // Inserindo no HTML da tabela
-  tabelaListaConvidados.innerHTML = `
+  tabelaListaConvidados.innerHTML += `
     <tr>
         <td>${convidado}</td>
         <td><button type="button" class="btn btn-danger" name='botao-remover'>
@@ -21,8 +26,10 @@ function insereConvidadoNoHTML (convidado) {
     `
 }
 
-/* Função que vai exibir os usuários no HTML quando chamada */
+/* Função que vai exibir os usuários no HTML quando chamada 1.0 Quando eu não manjo do foreach*/
 function exibeListaDeConvidados (lista) {
+  // Dando descarga no meu vaso
+  tabelaListaConvidados.innerHTML = ''
   // Percorrendo a lista pré-criada
   for (const convidado of lista) {
     // Chamando a função para inserir o convidado no html
@@ -32,4 +39,33 @@ function exibeListaDeConvidados (lista) {
   }
 }
 
-exibeListaDeConvidados(listaDeConvidados)
+/* Usando foreach para percorrer um array */ 
+function exibeListaDeConvidadosV2 () {
+    // Limpo a tabela HTML
+    tabelaListaConvidados.innerHTML = ''
+    // Percorro o array inserindo cada posição em um elemento do HTML (TR) na table
+    listaDeConvidados.forEach(convidado => insereConvidadoNoHTML(convidado))
+}
+
+/* Setando um atributo para esconder*/
+function fechaDivMensagemUsuario(){
+    divMensagemUsuario.setAttribute('hidden','')
+}
+
+exibeListaDeConvidadosV2(listaDeConvidados)
+
+/* Escutando o evento de click para disparar uma função quando ele acontecer */
+botaoIncluirConvidado.onclick = function(){
+    if(inputNomeConvidado.value.trim()){
+        listaDeConvidados.push(inputNomeConvidado.value)
+        exibeListaDeConvidadosV2(listaDeConvidados)
+        inputNomeConvidado.value = ''
+        divMensagemUsuario.removeAttribute('hidden')
+        // Confiro se to fazendo cagada ou não
+        console.log(listaDeConvidados)
+    }else{
+        // Exemplo de getAttribute
+        console.log(divMensagemUsuario.getAttribute('class'))
+    }
+    console.log('A treta ta on!')
+}
